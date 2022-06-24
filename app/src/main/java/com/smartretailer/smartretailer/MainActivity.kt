cf256c6971edb4817bff1fbef7aa730ea9dad5fd
AhmedAttya <Ahmed_Attya2011@outlook.com>
2022-06-24 13:34:43 +0200

    removed unused code and legacy support library

--- app/src/main/java/com/smartretailer/smartretailer/MainActivity.kt
+++ app/src/main/java/com/smartretailer/smartretailer/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.smartretailer.smartretailer
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import androidx.navigation.fragment.findNavController
 import com.smartretailer.smartretailer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,15 +20,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
-        val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main)
-
-      //  val navController = findNavController(R.id.nav_host_fragment_content_main)
-        val navController=   navHostFragment!!.findNavController()
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main)
+        val navController = navHostFragment!!.findNavController()
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
