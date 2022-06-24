40e151e85b01e537c74f9b04453454a37ab29dbe
AhmedAttya <Ahmed_Attya2011@outlook.com>
2022-06-24 13:05:13 +0200

    refactor sign up data class

--- app/src/main/java/com/smartretailer/smartretailer/helpers/Signup.kt
+++ app/src/main/java/com/smartretailer/smartretailer/helpers/Signup.kt
@@ -1,3 +1,9 @@
 package com.smartretailer.smartretailer.helpers
 
-data class Signup(var idToken:String, var email:String, var refreshToken:String, var expiresIn:String, var localId:String)
+data class Signup(
+    val idToken: String,
+    val email: String,
+    val refreshToken: String,
+    val expiresIn: String,
+    val localId: String,
+)
