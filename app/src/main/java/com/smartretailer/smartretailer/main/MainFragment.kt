5d1da15831c6a329e4d517c9f05bb6bda755e444
AhmedAttya <Ahmed_Attya2011@outlook.com>
2022-06-27 19:50:34 +0200

    refactored sign up and removed unused dimens values and redundant scrollview

--- app/src/main/java/com/smartretailer/smartretailer/main/MainFragment.kt
+++ app/src/main/java/com/smartretailer/smartretailer/main/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.smartretailer.smartretailer.databinding.FragmentMainBinding
@@ -26,6 +27,7 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        Toast.makeText(this.context, "reached maain", Toast.LENGTH_LONG).show()
 
 }
 }
